b79c65357e608d658aa418cf646d03894bd884d8
Rick Harris <rickharris@users.noreply.github.com>
2023-03-15 23:25:34 -0600

    disable bufferline; colorscheme and trouble tweaks

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -12,8 +12,6 @@ an executable
 vim.o.clipboard = ""
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = true
-vim.o.bg = "light"
-lvim.builtin.theme.name = "tokyonight"
 lvim.colorscheme = "solarized"
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
@@ -21,7 +19,7 @@ lvim.colorscheme = "solarized"
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 -- add your own keymapping
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+-- lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 -- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 -- unmap a default keymapping
@@ -56,31 +54,34 @@ end
 
 -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["b"] = {
-	e = { "<cmd>Telescope buffers sort_mru=1 theme=ivy previewer=false<cr>", "Buffer Explorer" },
+	e = { "<cmd>Telescope buffers sort_mru=1 ignore_current_buffer=1 color_devicons=0<cr>", "Buffer Explorer" },
 	o = { "<cmd>:BWipeout other<cr>", "Close other buffers" },
 }
+lvim.builtin.which_key.mappings["d"] = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "Trouble" }
 lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
-lvim.builtin.which_key.mappings["s"]["s"] = { "<cmd>Telescope neoclip layout=vertical<cr>", "Yank History" }
-lvim.builtin.which_key.mappings["s"]["e"] = {
+lvim.builtin.which_key.mappings["ss"] = { "<cmd>Telescope neoclip layout=vertical<cr>", "Yank History" }
+lvim.builtin.which_key.mappings["se"] = {
 	function()
 		require("spectre").open()
 	end,
 	"Find and Replace",
 }
-lvim.builtin.which_key.mappings["t"] = {
-	name = "+Trouble",
-	r = { "<cmd>Trouble lsp_references<cr>", "References" },
-	f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
-	d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
-	q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
-	l = { "<cmd>Trouble loclist<cr>", "LocationList" },
-	w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
-}
+
+-- lvim.builtin.which_key.mappings["t"] = {
+-- 	name = "+Trouble",
+-- 	r = { "<cmd>Trouble lsp_references<cr>", "References" },
+-- 	f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+-- 	d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+-- 	q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+-- 	l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+-- 	w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
+-- }
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.bufferline.active = false
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
