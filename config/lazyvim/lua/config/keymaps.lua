de77143244e71db3ef8e715f6ec3cb781d94f519
Rick Harris <rickharris@users.noreply.github.com>
2024-10-15 01:28:13 -0400

    vim-tmux-navigator for lazyvim

---
+++ config/lazyvim/lua/config/keymaps.lua
@@ -0,0 +1,9 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set("n", "<c-h>", "<cmd>TmuxNavigateLeft<cr>")
+vim.keymap.set("n", "<c-j>", "<cmd>TmuxNavigateDown<cr>")
+vim.keymap.set("n", "<c-k>", "<cmd>TmuxNavigateUp<cr>")
+vim.keymap.set("n", "<c-l>", "<cmd>TmuxNavigateRight<cr>")
+vim.keymap.set("n", "<c-\\>", "<cmd>TmuxNavigatePrevious<cr>")
