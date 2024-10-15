0f35d587cca91f09765417b76270055e7376ee90
Rick Harris <rickharris@users.noreply.github.com>
2024-10-14 23:16:24 -0400

    starship zsh prompt

--- config/lazyvim/lua/config/lazy.lua
+++ config/lazyvim/lua/config/lazy.lua
@@ -30,6 +30,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.refactoring" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     -- import/override with your plugins
