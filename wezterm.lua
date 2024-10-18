b3957e13f464ed074b7771fcefd069bd2ba38a80
Rick Harris <rickharris@users.noreply.github.com>
2024-10-18 01:03:36 -0700

    wezterm config_builder

--- wezterm.lua
+++ wezterm.lua
@@ -1,11 +1,15 @@
 local wezterm = require("wezterm")
+local config = wezterm.config_builder()
 
-return {
-  color_scheme = "tokyonight_storm",
-  enable_tab_bar = false,
-  font = wezterm.font("LetterGothicMono Nerd Font"),
-  font_size = 16,
-  window_background_opacity = 0.8,
-  macos_window_background_blur = 50,
-  window_decorations = "RESIZE",
-}
+-- Colors and fonts
+config.font = wezterm.font("LetterGothicMono Nerd Font")
+config.font_size = 18
+config.color_scheme = "tokyonight_storm"
+
+-- Window customizations
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 50
+
+return config
