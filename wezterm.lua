150cf823089174a092ae2843aae58e337b06d149
Rick Harris <rickharris@users.noreply.github.com>
2024-10-18 00:05:43 -0700

    wezterm

---
+++ wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require("wezterm")
+
+return {
+  color_scheme = "tokyonight_storm",
+  enable_tab_bar = false,
+  font = wezterm.font("LetterGothicMono Nerd Font"),
+  font_size = 16,
+  window_decorations = "RESIZE",
+}
