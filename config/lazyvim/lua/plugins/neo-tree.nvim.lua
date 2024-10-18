776ad4585c9ebf2e7e445cee31abebbf958d69a4
Rick Harris <rickharris@users.noreply.github.com>
2024-10-17 23:43:15 -0700

    neo-tree shows dotfiles and stays at cwd

---
+++ config/lazyvim/lua/plugins/neo-tree.nvim.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        bind_to_cwd = false,
+        filtered_items = {
+          hide_dotfiles = false,
+        },
+      },
+    },
+  },
+}
