f5321f30782fb8637683cb96eee77aefa6222967
Rick Harris <rickharris@users.noreply.github.com>
2024-10-19 03:47:38 -0700

    solarized

--- config/lazyvim/lua/plugins/colorscheme.lua
+++ config/lazyvim/lua/plugins/colorscheme.lua
@@ -1,29 +1,58 @@
 return {
   {
+    "maxmx03/solarized.nvim",
+    opts = {
+      palette = "solarized", -- solarized (default) | selenized
+      variant = "winter", -- "spring" | "summer" | "autumn" | "winter" (default)
+
+      styles = {
+        comments = { italic = true, bold = false },
+        visual = { bold = false },
+      },
+
+      on_highlights = function(colors)
+        local groups = {
+          -- Classic Solarized highlighting
+          -- Visual = {
+          --   bg = vim.o.background == "light" and colors.base1 or colors.base01,
+          --   fg = vim.o.background == "light" and colors.base3 or colors.base03,
+          --   bold = false,
+          -- },
+          -- More subtle visual highlight
+          Visual = {
+            bg = vim.o.bg == "light" and colors.base2 or colors.base02,
+            fg = "NONE",
+            bold = false,
+          },
+        }
+
+        return groups
+      end,
+    },
+  },
+  {
     "tokyonight.nvim",
     opts = {
       style = "storm",
     },
   },
-  -- { "EdenEast/nightfox.nvim" },
-  -- { "calind/selenized.nvim" },
-  -- {
-  --   "cormacrelf/dark-notify",
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     require("dark_notify").run({
-  --       schemes = {
-  --         dark = {
-  --           bg = "dark",
-  --           colorscheme = "tokyonight-storm",
-  --         },
-  --         light = {
-  --           bg = "light",
-  --           colorscheme = "dawnfox",
-  --         },
-  --       },
-  --     })
-  --   end,
-  -- },
+  {
+    "cormacrelf/dark-notify",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("dark_notify").run({
+        schemes = {
+          dark = {
+            bg = "dark",
+            colorscheme = "tokyonight-storm",
+          },
+          light = {
+            bg = "light",
+            colorscheme = "solarized",
+          },
+        },
+      })
+    end,
+  },
 }
--- wezterm.lua
+++ wezterm.lua
@@ -2,9 +2,25 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- Colors and fonts
+local function scheme_for_appearance(appearance)
+  if appearance:find("Dark") then
+    return "tokyonight_storm"
+  else
+    return "Builtin Solarized Light"
+  end
+end
+
+wezterm.on("window-config-reloaded", function(window)
+  local overrides = window:get_config_overrides() or {}
+  local appearance = window:get_appearance()
+  local scheme = scheme_for_appearance(appearance)
+  if overrides.color_scheme ~= scheme then
+    overrides.color_scheme = scheme
+    window:set_config_overrides(overrides)
+  end
+end)
 config.font = wezterm.font("LetterGothicMono Nerd Font")
 config.font_size = 18
-config.color_scheme = "tokyonight_storm"
 
 -- Window customizations
 config.enable_tab_bar = false
