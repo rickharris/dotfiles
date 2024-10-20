e1d9bba51f2a4b6a2cfb8c274e6d56a308e8484e
Rick Harris <rickharris@users.noreply.github.com>
2024-10-19 23:53:07 -0700

    extras

--- config/lazyvim/lua/plugins/extras.lua
+++ config/lazyvim/lua/plugins/extras.lua
@@ -1,6 +1,9 @@
 return {
   { import = "lazyvim.plugins.extras.coding.luasnip" },
+  { import = "lazyvim.plugins.extras.coding.mini-surround" },
+  { import = "lazyvim.plugins.extras.coding.yanky" },
   { import = "lazyvim.plugins.extras.editor.harpoon2" },
+  { import = "lazyvim.plugins.extras.editor.overseer" },
   { import = "lazyvim.plugins.extras.editor.refactoring" },
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.lang.toml" },
