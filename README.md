8b36031324c8f5b8fdb0916ded0ee1851d6def2e
Rick Harris <rickharris@users.noreply.github.com>
2024-10-20 22:09:56 -0700

    vscode and markdown

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Dotfiles
 
+```sh
 bash <(curl -s https://raw.githubusercontent.com/rickharris/dotfiles/main/bin/setup.sh)
+```
--- dotfiles/.config/nvim/lua/plugins/extras.lua
+++ dotfiles/.config/nvim/lua/plugins/extras.lua
@@ -6,8 +6,10 @@ return {
   { import = "lazyvim.plugins.extras.editor.overseer" },
   { import = "lazyvim.plugins.extras.editor.refactoring" },
   { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
   { import = "lazyvim.plugins.extras.lang.toml" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
   { import = "lazyvim.plugins.extras.linting.eslint" },
   { import = "lazyvim.plugins.extras.util.octo" },
+  { import = "lazyvim.plugins.extras.vscode" },
 }
