4c82b4cc1815ba93d4a1d41b1343df86d50e6dba
Rick Harris <rickharris@users.noreply.github.com>
2024-11-01 01:32:08 -0700

    diffview.nvim

--- dotfiles/.config/nvim/lua/plugins/git.lua
+++ dotfiles/.config/nvim/lua/plugins/git.lua
@@ -1,9 +1,21 @@
 return {
   {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "sindrets/diffview.nvim",
+    "sindrets/diffview.nvim",
+    lazy = true,
+    cmd = {
+      "DiffviewClose",
+      "DiffviewFileHistory",
+      "DiffviewFocusFiles",
+      "DiffviewLog",
+      "DiffviewOpen",
+      "DiffviewRefresh",
+      "DiffviewToggleFiles",
     },
+  },
+  {
+    "NeogitOrg/neogit",
+    lazy = true,
+    cmd = "Neogit",
     keys = {
       { "<leader>gg", "<cmd>Neogit<CR>", desc = "Neogit" },
     },
