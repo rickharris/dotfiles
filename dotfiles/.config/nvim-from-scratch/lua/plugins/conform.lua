1b1aec0b741bcd7e35260923a3ea50b4647e4dcc
Rick Harris <rickharris@users.noreply.github.com>
2024-11-03 19:22:49 -0800

    mason-tool-installer

---
+++ dotfiles/.config/nvim-from-scratch/lua/plugins/conform.lua
@@ -0,0 +1,17 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  ---@module "conform"
+  ---@type conform.setupOpts
+  opts = {
+    formatters_by_ft = {},
+    default_format_opts = {
+      lsp_format = "fallback",
+    },
+    format_on_save = { timeout_ms = 500 },
+  },
+  init = function()
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
+}
--- dotfiles/.config/nvim-from-scratch/lua/plugins/lua.lua
+++ dotfiles/.config/nvim-from-scratch/lua/plugins/lua.lua
@@ -1,12 +1,14 @@
 return {
   {
-    "williamboman/mason.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    event = "VimEnter",
     opts = {
       ensure_installed = { "stylua" },
     },
   },
   {
     "stevearc/conform.nvim",
+    dependencies = { "mason.nvim" },
     opts = {
       formatters_by_ft = {
         lua = { "stylua" },
