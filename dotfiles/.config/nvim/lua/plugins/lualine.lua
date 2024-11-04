c7e485d773a2b368828fef2b226f04b45f8a5cd2
Rick Harris <rickharris@users.noreply.github.com>
2024-11-04 13:55:45 -0800

    lualine customizations

---
+++ dotfiles/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        component_separators = { left = "|", right = "|" },
+        section_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_b = {
+          {
+            "branch",
+            icon = "",
+          },
+        },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+    },
+  },
+}
