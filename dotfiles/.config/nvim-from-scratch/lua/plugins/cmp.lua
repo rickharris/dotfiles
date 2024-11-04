f59e1348270971760422b89989527e22d3d413d0
Rick Harris <rickharris@users.noreply.github.com>
2024-11-03 23:14:48 -0800

    nvim-cmp

---
+++ dotfiles/.config/nvim-from-scratch/lua/plugins/cmp.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+    },
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.snippet = {
+        expand = function(args)
+          vim.snippet.expand(args.body)
+        end,
+      }
+      opts.mapping = cmp.mapping.preset.insert({
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+      })
+      opts.sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+      }, {
+        { name = "buffer" },
+      })
+    end,
+  },
+}
