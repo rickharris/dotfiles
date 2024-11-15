764d1bfe8c3e512f91fc08b3dfbcf4a631b07cb6
Rick Harris <rickharris@users.noreply.github.com>
2024-11-15 00:56:51 -0800

    add edgy.nvim

---
+++ dotfiles/.config/nvim/lua/plugins/edgy.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/edgy.nvim",
+    optional = true,
+    ---@module 'edgy'
+    ---@type Edgy.Config
+    opts = {
+      animate = {
+        enabled = false,
+      },
+    },
+  },
+}
