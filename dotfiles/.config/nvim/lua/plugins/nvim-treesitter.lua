5c78294f44ab03c1ba288f8793d0b4ab6d23eb2d
Rick Harris <rickharris@users.noreply.github.com>
2025-05-25 22:46:19 -0700

    LazyVim health check

---
+++ dotfiles/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "css",
+      "norg",
+      "scss",
+      "svelte",
+      "typst",
+      "vue",
+    },
+  },
+}
