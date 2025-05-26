de4301507094d8753c3284e21643d8a1ab4238f9
Rick Harris <rickharris@users.noreply.github.com>
2025-05-25 20:51:18 -0700

    Add back default sh formatter shfmt

--- dotfiles/.config/nvim/lua/plugins/conform.lua
+++ dotfiles/.config/nvim/lua/plugins/conform.lua
@@ -4,7 +4,7 @@ return {
     opts = {
       formatters_by_ft = {
         css = { "prettier", "stylelint" },
-        sh = { "shellcheck" },
+        sh = { "shellcheck", "shfmt" },
       },
     },
   },
