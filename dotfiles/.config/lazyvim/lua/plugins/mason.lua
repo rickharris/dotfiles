c1b61caf188e869c03cd5a34587b33a82e4ee2b2
Rick Harris <rickharris@users.noreply.github.com>
2025-07-05 15:02:02 -0700

    Add deno to lazyvim and asdf

--- dotfiles/.config/lazyvim/lua/plugins/mason.lua
+++ dotfiles/.config/lazyvim/lua/plugins/mason.lua
@@ -7,6 +7,7 @@ return {
         "css-lsp",
         "cssmodules-language-server",
         "css-variables-language-server",
+        "deno",
         "stylelint",
         "stylelint-lsp",
       },
