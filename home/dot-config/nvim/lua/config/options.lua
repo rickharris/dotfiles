57236fc98e032d102ddf3a58ac0b51414dba8e51
Rick Harris <rickharris@users.noreply.github.com>
2025-07-05 18:06:00 -0700

    nvim: mini.files

--- home/dot-config/nvim/lua/config/options.lua
+++ home/dot-config/nvim/lua/config/options.lua
@@ -1,4 +1,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+vim.opt.expandtab = true
 vim.opt.relativenumber = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
---
+++ home/dot-config/nvim/lua/plugins/mini-files.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    'echasnovski/mini.files',
+    version = '*',
+    opts = {
+      windows = {
+        preview = false,
+      },
+    },
+    keys = {
+      {
+        "<leader>e",
+        function()
+          require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+        end,
+        desc = "Open mini.files (Directory of Current File)",
+      },
+    },
+  }
+}
