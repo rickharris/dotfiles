8568b4307b03fe4a8fa55631d9fb5c3048ae4a2c
Rick Harris <rickharris@users.noreply.github.com>
2025-07-05 22:31:45 -0700

    nvim: javascript

--- home/dot-config/nvim/lua/plugins/lspconfig.lua
+++ home/dot-config/nvim/lua/plugins/lspconfig.lua
@@ -15,6 +15,7 @@ return {
   },
   {
     "mason-org/mason-lspconfig.nvim",
+    opts_extend = { "ensure_installed" },
     opts = {},
     dependencies = {
       { "mason-org/mason.nvim", opts = {} },
