40d8c77531715fafc4b1e7c53298a53ef92961f7
Rick Harris <rickharris@users.noreply.github.com>
2025-07-05 22:31:45 -0700

    nvim: javascript

--- home/dot-config/nvim/lua/plugins/treesitter.lua
+++ home/dot-config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,19 @@
+--- @type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
   version = "*",
-  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+  build = ":TSUpdate",
+  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall", "TSConfigInfo" },
+  opts_extend = { "ensure_installed" },
+  --- @module 'nvim-treesitter'
+  --- @type TSConfig
+  --- @diagnostic disable-next-line: missing-fields
+  opts = {
+    highlight = { enable = true },
+    indent = { enable = true },
+  },
+  ---@param opts TSConfig
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
