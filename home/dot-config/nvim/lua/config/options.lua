6791d346de8a25bb2de16b9f7b4befbf6f6825d5
Rick Harris <rickharris@users.noreply.github.com>
2025-07-06 23:15:27 -0700

    nvim: cursorline

--- home/dot-config/nvim/lua/config/options.lua
+++ home/dot-config/nvim/lua/config/options.lua
@@ -1,7 +1,9 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+vim.opt.cursorline = true
 vim.opt.expandtab = true
+vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
