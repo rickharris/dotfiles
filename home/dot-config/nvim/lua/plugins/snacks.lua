4a01b67bca1aad964e2f48a8175ab035abb13af1
Rick Harris <rickharris@users.noreply.github.com>
2025-07-07 16:02:41 -0700

    nvim: snacks dashboard and gitbrowse

--- home/dot-config/nvim/lua/plugins/snacks.lua
+++ home/dot-config/nvim/lua/plugins/snacks.lua
@@ -1,11 +1,14 @@
---- @type LazySpec
+---@type LazySpec
 return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
-  --- @module 'snacks'
-  --- @type snacks.Config
+  ---@module 'snacks'
+  ---@type snacks.Config
   opts = {
+    dashboard = {
+      enabled = true,
+    },
     notifier = {
       enabled = true,
     },
@@ -25,6 +28,28 @@ return {
         Snacks.picker.smart()
       end,
       desc = "Smart Find Files",
+    },
+    {
+      "<leader>go",
+      function()
+        Snacks.gitbrowse()
+      end,
+      mode = { "n", "x" },
+      desc = "Git Browse (open)",
+    },
+    {
+      "<leader>gy",
+      function()
+        Snacks.gitbrowse({
+          open = function(url)
+            vim.fn.setreg("+", url)
+          end,
+          notify = false,
+          what = "permalink",
+        })
+      end,
+      mode = { "n", "x" },
+      desc = "Git Browse (copy)",
     },
   },
 }
