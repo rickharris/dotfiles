defadec6c05f17d34bac484d77274c8bbdbe6660
Rick Harris <rickharris@users.noreply.github.com>
2025-07-07 14:16:57 -0700

    nvim: lualine

--- home/dot-config/nvim/lua/config/options.lua
+++ home/dot-config/nvim/lua/config/options.lua
@@ -3,9 +3,11 @@ vim.g.maplocalleader = "\\"
 
 vim.opt.cursorline = true
 vim.opt.expandtab = true
+vim.opt.laststatus = 3
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 2
+vim.opt.showmode = false
 vim.opt.tabstop = 2
 vim.opt.undofile = true
 vim.opt.undolevels = 10000
