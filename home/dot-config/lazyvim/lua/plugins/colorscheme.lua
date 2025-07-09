480bffadeb5996c81fa798d4a22b270583c610cf
Rick Harris <rickharris@users.noreply.github.com>
2025-07-08 19:23:47 -0700

    terminal light mode w/ dawnfox

--- home/dot-config/lazyvim/lua/plugins/colorscheme.lua
+++ home/dot-config/lazyvim/lua/plugins/colorscheme.lua
@@ -8,12 +8,13 @@ local colorschemes = {
   light = "dawnfox",
 }
 
-------@param bg "dark" | "light"
----local set_colorscheme = function(bg)
----  vim.api.nvim_set_option_value("background", bg, {})
----  vim.cmd("colorscheme " .. colorschemes[bg])
----end
+---@param bg "dark" | "light"
+local set_colorscheme = function(bg)
+  vim.api.nvim_set_option_value("background", bg, {})
+  vim.cmd("colorscheme " .. colorschemes[bg])
+end
 
+---@type LazySpec
 return {
   {
     "catppuccin/nvim",
@@ -123,22 +124,22 @@ return {
     dependencies = "rktjmp/lush.nvim",
     lazy = true,
   },
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = colorschemes.dark,
-  --   },
-  -- },
-  -- {
-  --   "f-person/auto-dark-mode.nvim",
-  --   opts = {
-  --     update_interval = 1000,
-  --     set_dark_mode = function()
-  --       set_colorscheme("dark")
-  --     end,
-  --     set_light_mode = function()
-  --       set_colorscheme("light")
-  --     end,
-  --   },
-  -- },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = colorschemes.dark,
+    },
+  },
+  {
+    "f-person/auto-dark-mode.nvim",
+    opts = {
+      update_interval = 1000,
+      set_dark_mode = function()
+        set_colorscheme("dark")
+      end,
+      set_light_mode = function()
+        set_colorscheme("light")
+      end,
+    },
+  },
 }
