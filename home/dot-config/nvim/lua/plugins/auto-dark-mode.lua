62b2634dc9fc3c5c33a4d074ad66227028170962
Rick Harris <rickharris@users.noreply.github.com>
2025-07-08 21:02:29 -0700

    nvim: dawnfox and auto-dark-mode

---
+++ home/dot-config/nvim/lua/plugins/auto-dark-mode.lua
@@ -0,0 +1,24 @@
+local colorschemes = {
+  dark = "tokyonight",
+  light = "dawnfox",
+}
+
+---@param bg "dark" | "light"
+local set_colorscheme = function(bg)
+  vim.api.nvim_set_option_value("background", bg, {})
+  vim.cmd("colorscheme " .. colorschemes[bg])
+end
+
+---@type LazySpec
+return {
+  "f-person/auto-dark-mode.nvim",
+  opts = {
+    update_interval = 1000,
+    set_dark_mode = function()
+      set_colorscheme("dark")
+    end,
+    set_light_mode = function()
+      set_colorscheme("light")
+    end,
+  },
+}
