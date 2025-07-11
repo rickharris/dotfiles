99dbd634cf736829c40ea676c1d5ce9e4cf1b74b
Rick Harris <rickharris@users.noreply.github.com>
2025-07-10 20:06:21 -0700

    nvim(nightfox): consistent floating window styles

--- home/dot-config/nvim/lua/plugins/nightfox.lua
+++ home/dot-config/nvim/lua/plugins/nightfox.lua
@@ -13,10 +13,28 @@ return {
     },
     palettes = {
       dawnfox = {
+        --- The default dawnfox yellow does not have enough contrast for my
+        --- eyes. These values are the colors from the theme, shaded down by
+        --- 0.25.
         yellow = {
           base = "#b07627",
           bright = "#b37e35",
           dim = "#a66c1b",
+        },
+      },
+    },
+    groups = {
+      all = {
+        --- Nightfox themes set `NormalFloat` to have a darker background color to contrast it to the standard background color, but
+        --- float border and title have the normal background. This results in
+        --- a weird two-tone theme for floating windows. I prefer to have
+        --- NormalFloat, FloatBorder, and FloatTitle to all have matching
+        --- styles.
+        FloatBorder = {
+          link = "NormalFloat",
+        },
+        FloatTitle = {
+          link = "NormalFloat",
         },
       },
     },
