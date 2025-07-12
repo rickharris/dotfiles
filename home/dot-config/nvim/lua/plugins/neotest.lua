2e01fff66fd1780d04207686f2950640d5fb3d1b
Rick Harris <rickharris@users.noreply.github.com>
2025-07-11 18:38:18 -0700

    nvim: neotest

---
+++ home/dot-config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,63 @@
+---@type LazySpec
+return {
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+    },
+    opts_extend = { "adapters" },
+    opts = {},
+    keys = {
+      {
+        "<leader>tt",
+        function()
+          require("neotest").run.run(vim.fn.expand("%"))
+        end,
+        desc = "Run File",
+      },
+      {
+        "<leader>tT",
+        function()
+          require("neotest").run.run(vim.uv.cwd())
+        end,
+        desc = "Run All Test Files",
+      },
+      {
+        "<leader>tr",
+        function()
+          require("neotest").run.run()
+        end,
+        desc = "Run Nearest",
+      },
+      {
+        "<leader>ts",
+        function()
+          require("neotest").summary.toggle()
+        end,
+        desc = "Toggle Summary",
+      },
+      {
+        "<leader>to",
+        function()
+          require("neotest").output.open({ enter = false, auto_close = true })
+        end,
+        desc = "Show Output",
+      },
+      {
+        "<leader>tO",
+        function()
+          require("neotest").output_panel.toggle()
+        end,
+        desc = "Toggle Output Panel",
+      },
+      {
+        "<leader>tS",
+        function()
+          require("neotest").run.stop()
+        end,
+        desc = "Stop",
+      },
+    },
+  },
+  { "nvim-lua/plenary.nvim", lazy = true },
+}
