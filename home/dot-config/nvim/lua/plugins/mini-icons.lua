07d8c8abd52f1f690619226f106971db0f1d5a12
Rick Harris <rickharris@users.noreply.github.com>
2025-07-18 01:31:08 -0700

    nvim: fix icons for plugins that use nvim-web-devicons

--- home/dot-config/nvim/lua/plugins/mini-icons.lua
+++ home/dot-config/nvim/lua/plugins/mini-icons.lua
@@ -1,4 +1,14 @@
 ---@type LazySpec
 return {
-  { "echasnovski/mini.icons", version = "*", lazy = true },
+  {
+    "echasnovski/mini.icons",
+    version = "*",
+    lazy = true,
+    init = function()
+      package.preload["nvim-web-devicons"] = function()
+        require("mini.icons").mock_nvim_web_devicons()
+        return package.loaded["nvim-web-devicons"]
+      end
+    end,
+  },
 }
