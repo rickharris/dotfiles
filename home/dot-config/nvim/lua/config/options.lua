7fbb849fb9875cccb061930c43d6acb354bf9a30
Rick Harris <rickharris@users.noreply.github.com>
2025-11-25 19:29:21 -0800

    nvim: keymap for interacting with system clipboard

--- home/dot-config/nvim/lua/config/options.lua
+++ home/dot-config/nvim/lua/config/options.lua
@@ -18,6 +18,11 @@ vim.opt.tabstop = 2
 vim.opt.undofile = true
 vim.opt.undolevels = 10000
 
+-- Make copying and pasting from clipboard a little easier
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
+vim.keymap.set({ "n", "v" }, "<leader>p", '"+p')
+vim.keymap.set({ "n", "v" }, "<leader>P", '"+P')
+
 vim.filetype.add({
   pattern = {
     [".*/ghostty/config"] = "conf",
