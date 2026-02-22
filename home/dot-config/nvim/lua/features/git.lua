f164ed55f4fdbe81c8c9bf7c0123e1ba83858da9
Rick Harris <rickharris@users.noreply.github.com>
2026-02-22 13:38:06 -0800

    nvim: add gh.nvim

---
+++ home/dot-config/nvim/lua/features/git.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = {
+      ensure_installed = {
+        "git_config",
+        "gitcommit",
+        "git_rebase",
+        "gitignore",
+        "gitattributes",
+      },
+    },
+  },
+}
