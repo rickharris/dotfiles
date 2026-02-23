172e92859f8738d5822bf0ed2b2214d86dd49cc6
Rick Harris <rickharris@users.noreply.github.com>
2026-02-23 02:17:54 -0800

    lazyvim: ensure codediff overrides builtin snacks keybindings

--- home/dot-config/lazyvim/lua/plugins/codediff.lua
+++ home/dot-config/lazyvim/lua/plugins/codediff.lua
@@ -1,14 +1,22 @@
+local keys = {
+  {
+    "<leader>gd",
+    "<cmd>CodeDiff<cr>",
+    desc = "Diff view",
+  },
+  {
+    "<leader>gD",
+    "<cmd>CodeDiff main...<cr>",
+    desc = "Diff view (merge base)",
+  },
+}
+
 ---@type LazySpec
 return {
   "esmuellert/codediff.nvim",
+  lazy = true,
   cmd = "CodeDiff",
-  keys = {
-    {
-      "<leader>gd",
-      "<cmd>CodeDiff<cr>",
-      desc = "Diff view",
-    },
-  },
+  keys = keys,
   specs = {
     {
       "NeogitOrg/neogit",
@@ -16,6 +24,13 @@ return {
       ---@module 'neogit'
       ---@type NeogitConfig
       opts = { integrations = { codediff = true } },
+    },
+    {
+      {
+        "folke/snacks.nvim",
+        optional = true,
+        keys = keys, -- Ensure default snacks keybindings get overridden by our keybindings
+      },
     },
   },
 }
