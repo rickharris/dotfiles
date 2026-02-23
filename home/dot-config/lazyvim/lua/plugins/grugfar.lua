4add8925c53022edb3071157ecb936f4f9910977
Rick Harris <rickharris@users.noreply.github.com>
2026-02-23 01:31:43 -0800

    lazyvim: grug-far customizations

---
+++ home/dot-config/lazyvim/lua/plugins/grugfar.lua
@@ -0,0 +1,29 @@
+---@type LazySpec
+return {
+  "MagicDuck/grug-far.nvim",
+  cmd = { "GrugFar", "GrugFarWithin" },
+  ---@class (partial) grug.far.Options
+  opts = {
+    transient = true,
+  },
+  keys = {
+    {
+      "<leader>sr",
+      function()
+        ---@class grug.far.OptionsOverride
+        local opts = {
+          visualSelectionUsage = "operate-within-range",
+        }
+
+        if vim.bo.filetype == "snacks_dashboard" then
+          -- When triggered from the dashboard, open grug-far in the current window.
+          opts.windowCreationCommand = ""
+        end
+
+        require("grug-far").open(opts)
+      end,
+      mode = { "n", "x" },
+      desc = "Search and Replace",
+    },
+  },
+}
