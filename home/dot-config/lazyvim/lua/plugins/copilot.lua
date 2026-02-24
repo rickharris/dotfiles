00ab4737078f4d46bbc11532b78baa5c9bcff5ea
Rick Harris <rickharris@users.noreply.github.com>
2026-02-24 14:41:21 -0800

    revert to nvm for node version management

--- home/dot-config/lazyvim/lua/plugins/copilot.lua
+++
@@ -1,9 +0,0 @@
----@type LazySpec
-return {
-  "zbirenbaum/copilot.lua",
-  optional = true,
-  ---@class (partial) CopilotConfig
-  opts = {
-    copilot_node_command = vim.fn.expand("$HOME") .. "/.asdf/installs/nodejs/lts/bin/node",
-  },
-}
