b2a7b2f129efa8a554a2ce846e7c960135cdf26b
Rick Harris <rickharris@users.noreply.github.com>
2026-03-15 15:22:00 -0700

    nvim: rzip for yarn pnp

---
+++ home/dot-config/lazyvim/lua/plugins/rzip.lua
@@ -0,0 +1,9 @@
+--!strict
+
+---@module 'lazy'
+---@type LazySpec
+return {
+  -- For yarn pnp, in theory, although I haven't ever actually gotten this to work.
+  -- https://yarnpkg.com/getting-started/editor-sdks#neovim-native-lsp
+  "lbrayner/vim-rzip",
+}
