03dac996380e7c361df59053da12a69121c5026b
Rick Harris <rickharris@users.noreply.github.com>
2026-03-18 19:12:04 -0700

    astronvim: try it out

---
+++ home/dot-config/astronvim/README.md
@@ -0,0 +1,5 @@
+# AstroNvim Template
+
+**NOTE:** This is for AstroNvim v5+
+
+A template for getting started with [AstroNvim](https://github.com/AstroNvim/AstroNvim)
---
+++ home/dot-config/astronvim/lua/community.lua
@@ -0,0 +1,41 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.ai.sidekick-nvim" },
+  { import = "astrocommunity.bars-and-lines.vim-illuminate" },
+  { import = "astrocommunity.code-runner.overseer-nvim" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  { import = "astrocommunity.colorscheme.lackluster-nvim" },
+  { import = "astrocommunity.colorscheme.mellifluous-nvim" },
+  { import = "astrocommunity.colorscheme.nightfox-nvim" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.colorscheme.zenbones" },
+  { import = "astrocommunity.completion.copilot-lua" },
+  { import = "astrocommunity.debugging.nvim-dap-repl-highlights" },
+  { import = "astrocommunity.debugging.nvim-dap-view" },
+  { import = "astrocommunity.debugging.nvim-dap-virtual-text" },
+  { import = "astrocommunity.git.codediff-nvim" },
+  { import = "astrocommunity.git.neogit" },
+  { import = "astrocommunity.lsp.ts-error-translator-nvim" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.eslint" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.programming-language-support.kulala-nvim" },
+  { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
+  { import = "astrocommunity.recipes.vscode" },
+  { import = "astrocommunity.search.grug-far-nvim" },
+  { import = "astrocommunity.test.neotest" },
+  { import = "astrocommunity.test.nvim-coverage" },
+  -- import/override with your plugins folder
+}
