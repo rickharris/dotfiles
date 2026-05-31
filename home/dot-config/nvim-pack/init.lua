8f953cb7e0a6862eeafac62d4fa2b1c5a8f75a65
Rick Harris <rickharris@users.noreply.github.com>
2026-05-30 23:48:10 -0700

    nvim-pack: add more base vim options

--- home/dot-config/nvim-pack/init.lua
+++ home/dot-config/nvim-pack/init.lua
@@ -4,6 +4,21 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 vim.o.number = true
+vim.o.breakindent = true
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.signcolumn = "yes"
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.o.inccommand = "split"
+vim.o.cursorline = true
+vim.o.scrolloff = 10
+vim.o.confirm = true
+
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 vim.pack.add({
   "https://github.com/esmuellert/codediff.nvim",
